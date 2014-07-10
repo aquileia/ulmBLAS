33f216c77fbe4c01106f53b7c916f663b2497bd0
aquileia <sk.aquileia@gmail.com>
2014-07-10 16:55:15 +0200

    Implement dtrsm for upper triangular matrices

--- src/level3/dtrsm_u.c
+++ src/level3/dtrsm_u.c
@@ -1,5 +1,6 @@
 #include <ulmblas.h>
 #include <math.h>
+#include <level1/dgescal.h>
 
 //
 //  Solve A*X = alpha*B
@@ -21,4 +22,28 @@ dtrsm_u(enum Diag       diag,
         int             incColB)
 {
     // Your implementation goes here
+    int i,j,k;
+
+    //nothing to be done
+    if (n==0 || m==0) return;
+
+    dgescal(m,n,alpha,B,incRowB, incColB);
+
+    //for alpha == 0, we return X = 0
+    if (alpha==0.0) return;
+
+    for (j=0; j<n; ++j) {
+        for (k=m-1; k>=0; --k) {
+            //empty entry: x_{jk} = 0
+            if (B[k*incRowB + j*incColB] == 0.0) continue;
+            if (diag!=Unit) {
+                B[k*incRowB + j*incColB] /= A[k*incRowA +k*incColA];
+            }
+            for (i=0; i<k; ++i) {
+                B[i*incRowB + j*incColB] -= B[k*incRowB + j*incColB]
+                                          * A[i*incRowA +k*incColA];
+
+            }
+        }
+    }
 }
